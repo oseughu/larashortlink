3c70d73513d5a9a59fa8b06139c218a03ee71b75
Osemudiamhen Ughu <oseughu@gmail.com>
2024-12-30 16:37:30 +0000

    update api root route

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UrlShortenerController;
 
+Route::get('/', function () {
+    return response()->json(['message' => 'Welcome to my Laravel Link Shortener!']);
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
